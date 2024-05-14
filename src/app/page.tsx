7c1aaaef602db6a415e7b431cb7b68fead34411d
axai-kaizoku <02b3akshay@gmail.com>
2024-05-14 15:09:45 +0530

    use auth for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { headers } from "next/headers";
 import { db } from "~/server/db";
 
-export default async function HomePage() {
-  headers();
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
-
   return (
-    <main>
+    <>
       <div className="flex flex-wrap gap-4">
         {[...images, ...images, ...images].map((image, index) => (
           <div className="flex w-48 flex-col" key={image.id + "" + index}>
@@ -17,6 +16,23 @@ export default async function HomePage() {
           </div>
         ))}
       </div>
+    </>
+  );
+}
+
+export default async function HomePage() {
+  headers();
+
+  return (
+    <main>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
